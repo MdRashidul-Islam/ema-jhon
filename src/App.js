061b337d9a0779a396cbb50e916a9d6f6ed5153b
rashidulrony <rashidulrony79775@gmail.com>
2021-10-13 15:30:23 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import Header from "./components/Header/Header";
 import Shop from "./components/Shop/Shop";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Review from "./components/Review/Review";
+import Inventory from "./components/Inventory/Inventory";
 
 
 function App() {
   return (
     <div className="App">
-     <Header></Header>
-     <Shop></Shop>
+      <Router>
+      <Header></Header>
+        <Switch>
+          <Route exact path="/">
+          <Shop></Shop>
+          </Route>
+          <Route path="/shop">
+          <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            <Review></Review>
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route exact path="*">
+            <h1>404</h1>
+          </Route>
+        </Switch>
+      </Router>
+    
+    
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,36 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 import logo from '../../images/logo.png'
 import './Header.css'
 
 
 const Header = () => {
         return (
-                <div>
-                   <img className="logo" src={logo} alt="" />    
-                <nav>
-                <a href="/shop">Shop</a>
-                <a href="/review">Order Review</a>
-                <a href="/inventory">Inventory</a>
-                           
-                </nav> 
-                </div>
-        );
+<div>
+        <img className="logo" src={logo} alt="" />  
+        <Navbar bg="light" expand="lg">
+        <Container fluid>
+        <Navbar.Brand href="#"><img width="150px" src={logo} alt="" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+        <Nav
+                className="ms-auto my-2 my-lg-0"
+                style={{ maxHeight: '100px' }}
+                navbarScroll
+        >
+               
+
+                <NavLink to="/shop">Shop</NavLink>
+                <NavLink to="/review">Order Review</NavLink>
+                <NavLink to="/inventory">Inventory</NavLink>
+        </Nav>
+        </Navbar.Collapse>
+        </Container>
+        </Navbar>
+
+</div>
+);
 };
 
 export default Header;

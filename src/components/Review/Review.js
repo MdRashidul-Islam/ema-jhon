3052d2588c1b9f353e651249b57668e8270930cb
rashidulrony <rashidulrony79775@gmail.com>
2021-10-13 16:02:32 +0600

    update

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
+import useProducts from '../../hooks/useProducts';
 
 const Review = () => {
+        const [products]= useProducts()
+        const [cart]=useCart(products)
         return (
                 <div>
-                    <h1>this is order review</h1>    
+                    <h1>{products.length}</h1>    
                 </div>
         );
 };
